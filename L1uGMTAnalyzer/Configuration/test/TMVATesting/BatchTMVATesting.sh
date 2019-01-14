f03cc56eec9c666a63205c7cb1fb64753cb18645
Emmanouil Vourliotis <emmanouil.vourliotis@cern.ch>
2019-01-14 12:59:34 +0000

    Update BatchTMVATesting.sh

--- L1uGMTAnalyzer/Configuration/test/TMVATesting/BatchTMVATesting.sh
+++ L1uGMTAnalyzer/Configuration/test/TMVATesting/BatchTMVATesting.sh
@@ -1,4 +1,4 @@
 cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
-cd $CMSSW_BASE/src/L1uGMTAnalyzer/Configuration/test/TMVA_PhiExtrapolation_New
+cd $CMSSW_BASE/src/L1uGMTAnalyzer/Configuration/test/TMVATesting/
 root -l -b -q TMVARegReader_Impr_Runner.C
