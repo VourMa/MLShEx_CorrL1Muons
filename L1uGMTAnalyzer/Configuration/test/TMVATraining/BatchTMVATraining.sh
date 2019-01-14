000752a421da38baebd657c257fcabc97decf820
Emmanouil Vourliotis <emmanouil.vourliotis@cern.ch>
2019-01-14 13:00:27 +0000

    Update BatchTMVATraining.sh

--- L1uGMTAnalyzer/Configuration/test/TMVATraining/BatchTMVATraining.sh
+++ L1uGMTAnalyzer/Configuration/test/TMVATraining/BatchTMVATraining.sh
@@ -1,6 +1,6 @@
 cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
-cd $CMSSW_BASE/src/L1uGMTAnalyzer/Configuration/test/TMVA_PhiExtrapolation_New
+cd $CMSSW_BASE/src/L1uGMTAnalyzer/Configuration/test/TMVATraining/
 root -l -b -q TMVARegression_Impr_00_08.C
 root -l -b -q TMVARegression_Impr_08_12.C
 root -l -b -q TMVARegression_Impr_12_24.C
