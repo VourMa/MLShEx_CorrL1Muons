2e1b3b025e2e19f861f49db9a997d94d149b1567
Sotiroulla Konstantinou <skonstan@cern.ch>
2019-06-22 20:07:58 +0200

    skimming: muon properties are stored in variables. Vectors are filled only if onlytxt is disabled

--- Configuration/test/skimming/L1toRecoMatcher.C
+++ Configuration/test/skimming/L1toRecoMatcher.C
@@ -67,7 +67,7 @@ void L1toRecoMatcher::Loop(TString ID,TString out, bool debug, bool onlytxt)
 	
 	Long64_t nentries = fChain->GetEntriesFast();
 	//cout << "Total number of events: " << nentries << endl;
-	if( debug == 1 ) nentries = 100000;	
+	if( debug == 1 ) nentries = 100000;
 	
 	//___Get the profile histograms___
 	TFile profFile("/eos/cms/store/cmst3/user/evourlio/L1uGMTAnalyzer_Trees/plots_tight_test_profile.root","read");
@@ -80,35 +80,17 @@ void L1toRecoMatcher::Loop(TString ID,TString out, bool debug, bool onlytxt)
 	outfile.open(out);	
 	std::cout<<"Created output file: "<<out<<std::endl;
 
-
-	//___Definitions__//
-	TTree * mytree =new TTree("mytree","mytree");
-
-	int event_ = 0, recomuon_N, L1muon_N;
+	//Write name of variables in txt file (only once)
+	outfile << std::setw(12) << "reco_pt"   << std::setw(12) << "reco_eta"  << std::setw(12)  << "reco_phi"       << std::setw(15) << "reco_etaAtVtx" <<  std::setw(15) << "reco_phiAtVtx"
+		<< std::setw(10) << "l1_pt"     << std::setw(10) << "l1_eta"    << std::setw(12)  << "l1_phi"         << std::setw(15) << "l1_etaAtVtx"   <<  std::setw(15) << "l1_phiAtVtx"
+		<< std::setw(12) << "l1_ptCorr" << std::setw(12) << "l1_charge" << std::setw(15)  << "l1_tfMuonIndex" << std::setw(10) << "l1_hwQual" 
+		<< std::setw(10) << "dr_map"    << std::setw(10) << "dphi" << std::endl;						
+	
+	//___Definitions__//	
+	int event_i;
+	std::vector <int> event_, recomuon_N, L1muon_N;
 	MatchedMuons recomuon, L1muon;
 	
-	mytree->Branch("event",&event_);
-	mytree->Branch("recomuon_N",&recomuon_N);
-	mytree->Branch("recomuon_pt",&recomuon.Pt);
-	mytree->Branch("recomuon_eta",&recomuon.Eta);
-	mytree->Branch("recomuon_phi",&recomuon.Phi);
-	mytree->Branch("recomuon_dr",&recomuon.Dr);
-	//mytree->Branch("recomuon_isTight",&recomuon.IsTight);
-	//mytree->Branch("recomuon_isMedium",&recomuon.IsMedium);
-	//mytree->Branch("recomuon_isLoose",&recomuon.IsLoose);
-	//mytree->Branch("recomuon_isGlobal",&recomuon.IsGlobal);
-	//mytree->Branch("recomuon_isStandAlone",&recomuon.IsStandAlone);
-	mytree->Branch("L1muon_N",&L1muon_N);
-	mytree->Branch("L1muon_pt",&L1muon.Pt);
-	mytree->Branch("L1muon_ptCorr",&L1muon.PtCorr);
-	mytree->Branch("L1muon_eta",&L1muon.Eta);
-	mytree->Branch("L1muon_etaAtVtx",&L1muon.EtaAtVtx);
-	mytree->Branch("L1muon_phi",&L1muon.Phi);
-	mytree->Branch("L1muon_phiAtVtx",&L1muon.PhiAtVtx);
-	mytree->Branch("L1muon_charge",&L1muon.Charge);
-	mytree->Branch("L1muon_tfMuonIndex",&L1muon.TfMuonIndex);
-	mytree->Branch("L1muon_hwQual",&L1muon.HwQual);
-
 	Long64_t nbytes = 0, nb = 0;
 	for (Long64_t jentry=0; jentry<nentries;jentry++) {
 		Long64_t ientry = LoadTree(jentry);
@@ -120,7 +102,6 @@ void L1toRecoMatcher::Loop(TString ID,TString out, bool debug, bool onlytxt)
 
 		int nReco = 0;
 		std::map<float, pair<int, int> > dr_reco_L1;
-		recomuon.Clear(); L1muon.Clear();
 
 
 		for(unsigned int i = 0; i < recomuon_pt->size(); i++) {
@@ -162,98 +143,130 @@ void L1toRecoMatcher::Loop(TString ID,TString out, bool debug, bool onlytxt)
 		
 		
 		for (map<float, pair<int, int> >::iterator it=dr_reco_L1.begin(); it!=dr_reco_L1.end(); it++) {
-			float dr_map = it->first;
-			int recoIndex_map = it->second.first;
-			int L1Index_map = it->second.second;
+		  float dr_map      = it->first;
+		  int recoIndex_map = it->second.first;
+		  int L1Index_map   = it->second.second;
 			
-			recomuon.Pt.push_back( recomuon_pt->at(recoIndex_map) );
-			recomuon.Eta.push_back( recomuon_eta->at(recoIndex_map) );
-			recomuon.Phi.push_back( recomuon_phi->at(recoIndex_map) );
-			recomuon.EtaAtVtx.push_back( recomuon_eta->at(recoIndex_map) );
-			recomuon.PhiAtVtx.push_back( recomuon_phi->at(recoIndex_map) );
-			recomuon.Dr.push_back( dr_map );
-			if(L1Index_map >= 0) {
-				L1muon.Pt.push_back( muon_pt->at(L1Index_map) );
-				L1muon.Eta.push_back( muon_eta->at(L1Index_map) );
-				
-				double bin, corrFactor = 2.0;
-				if( fabs( L1muon.Eta.back() ) <= 0.8)
-				  {
-				    bin = BMTF.FindBin( L1muon.Pt.back() );
-				    corrFactor = BMTF.GetBinContent( bin );
-				    if( corrFactor == 0.0 ) corrFactor = 2.0; //Correct for empty bins
-				  }
-				
-				else if( fabs( L1muon.Eta.back() ) <= 1.2)
-				  {
-				    bin = OMTF.FindBin( L1muon.Pt.back() );
-				    corrFactor = OMTF.GetBinContent( bin );
-				    if( corrFactor == 0.0 ) corrFactor = 2.0; //Correct for empty bins
-				  }
-				
-				else //( fabs( L1muon.Eta.back() ) > 1.2)
-				  {
-				    bin = EMTF.FindBin( L1muon.Pt.back() );
-				    corrFactor = EMTF.GetBinContent( bin );
-				    if( corrFactor == 0.0 ) corrFactor = 3.0; //Correct for empty bins
-				  }
-				L1muon.PtCorr.push_back( L1muon.Pt.back() / corrFactor );
-				//cout << "pt = " << L1muon.Pt.back() << " eta = " << L1muon.Eta.back() << endl;
-				//cout << "bin = " << bin << " corrFactor = " << corrFactor << endl;
-				//cout << "ptCorr = " << L1muon.PtCorr.back() << endl;
-				
-				L1muon.Phi.push_back( muon_phi->at(L1Index_map) );
-				L1muon.EtaAtVtx.push_back( muon_EtaAtVtx->at(L1Index_map) );
-				L1muon.PhiAtVtx.push_back( muon_PhiAtVtx->at(L1Index_map) );
-				L1muon.Charge.push_back( muon_charge->at(L1Index_map) );
-				L1muon.TfMuonIndex.push_back( muon_tfMuonIndex->at(L1Index_map) );
-				L1muon.HwQual.push_back( muon_hwQual->at(L1Index_map) );
-				
-				//recomuon_pt, recomuon_eta, recomuon_phi, dr_map, muon_pt, muon_ptCorr, muon_eta, muon_etaAtVtx, muon_phi, muon_phiAtVtx, muon_charge, muon_tfMuonIndex, muon_hwQual, DPhi(muon_PhiAtVtx->at(L1Index_map)
-				outfile << std::setw(12) << recomuon_pt->at(recoIndex_map)    << std::setw(12) << recomuon_eta->at(recoIndex_map) 
-					<< std::setw(12) << recomuon_phi->at(recoIndex_map)   << std::setw(12) << dr_map << std::setw(12) << muon_pt->at(L1Index_map) 
-					<< std::setw(12) << L1muon.Pt.back() / corrFactor     << std::setw(12) << muon_eta->at(L1Index_map)
-					<< std::setw(12) << muon_EtaAtVtx->at(L1Index_map)    << std::setw(12) << muon_phi->at(L1Index_map) 
-					<< std::setw(12) << muon_PhiAtVtx->at(L1Index_map)    << std::setw(12) << muon_charge->at(L1Index_map)   
-					<< std::setw(12) << muon_tfMuonIndex->at(L1Index_map) << std::setw(12) << muon_hwQual->at(L1Index_map)
-					<< std::setw(12) << DPhi(muon_PhiAtVtx->at(L1Index_map), recomuon_phi->at(recoIndex_map)) <<std::endl;
-			}
-			else {
-				L1muon.Pt.push_back( -100.0 );
-				L1muon.PtCorr.push_back( -100.0 );
-				L1muon.Eta.push_back( -100.0 );
-				L1muon.Phi.push_back( -100.0 );
-				L1muon.EtaAtVtx.push_back( -100.0 );
-				L1muon.PhiAtVtx.push_back( -100.0 );
-				L1muon.Charge.push_back( -100.0 );
-				L1muon.TfMuonIndex.push_back( -100.0 );
-				L1muon.HwQual.push_back( -100.0 );
+		  //Reco muon
+		  float reco_pt       = recomuon_pt->at(recoIndex_map);
+		  float reco_eta      = recomuon_eta->at(recoIndex_map);
+		  float reco_phi      = recomuon_phi->at(recoIndex_map);
+		  float reco_etaAtVtx = recomuon_eta->at(recoIndex_map);
+		  float reco_phiAtVtx = recomuon_phi->at(recoIndex_map);
+			
+		  //L1 muon
+		  float l1_pt, l1_eta, l1_ptCorr, l1_phi, l1_etaAtVtx, l1_phiAtVtx, l1_charge, l1_tfMuonIndex, l1_hwQual;
+		  l1_pt = l1_eta = l1_ptCorr = l1_phi = l1_etaAtVtx = l1_phiAtVtx = l1_charge = l1_tfMuonIndex = l1_hwQual = -100.0;
+			
+		  //DeltaPhi(l1muon, recomuon)
+		  float dphi = -100.0;
 
-				outfile << std::setw(12) << recomuon_pt->at(recoIndex_map)    << std::setw(12) << recomuon_eta->at(recoIndex_map) 
-					<< std::setw(12) << recomuon_phi->at(recoIndex_map)   << std::setw(12) << dr_map 
-					<< std::setw(12) << -100.0 << std::setw(12) << -100.0 << std::setw(12) << -100.0 << std::setw(12) << -100.0 
-					<< std::setw(12) << -100.0 << std::setw(12) << -100.0 << std::setw(12) << -100.0 << std::setw(12) << -100.0 
-					<< std::setw(12) << -100.0 << std::setw(12) << -100.0 << std::endl;
-			}
-			recomuon.Charge.push_back( L1muon.Charge.back() );
-			recomuon.HwQual.push_back( L1muon.HwQual.back() );
+		  if(L1Index_map >= 0) {
+		    l1_pt          = muon_pt->at(L1Index_map);
+		    l1_eta         = muon_eta->at(L1Index_map);
+		    l1_phi         = muon_phi->at(L1Index_map);
+		    l1_etaAtVtx    = muon_EtaAtVtx->at(L1Index_map);
+		    l1_phiAtVtx    = muon_PhiAtVtx->at(L1Index_map);
+		    l1_charge      = muon_charge->at(L1Index_map);
+		    l1_tfMuonIndex = muon_tfMuonIndex->at(L1Index_map);
+		    l1_hwQual      = muon_hwQual->at(L1Index_map);
+
+		    dphi = DPhi(muon_PhiAtVtx->at(L1Index_map), recomuon_phi->at(recoIndex_map));
+			  
+		    double bin, corrFactor = 2.0;
+		    if( fabs( l1_eta ) <= 0.8)
+		      {
+			bin = BMTF.FindBin( l1_pt );
+			corrFactor = BMTF.GetBinContent( bin );
+			if( corrFactor == 0.0 ) corrFactor = 2.0; //Correct for empty bins
+		      }
+				
+		    else if( fabs( l1_eta ) <= 1.2)
+		      {
+			bin = OMTF.FindBin( l1_pt );
+			corrFactor = OMTF.GetBinContent( bin );
+			if( corrFactor == 0.0 ) corrFactor = 2.0; //Correct for empty bins
+		      }
+				
+		    else //( fabs( l1_eta ) > 1.2)
+		      {
+			bin = EMTF.FindBin( l1_pt );
+			corrFactor = EMTF.GetBinContent( bin );
+			if( corrFactor == 0.0 ) corrFactor = 3.0; //Correct for empty bins
+		      }
+			  
+		    l1_ptCorr = l1_pt / corrFactor;
+		  } //if(L1Index_map >= 0)
+			
+		  //Store variables in txt file
+		  // reco_pt, reco_eta, reco_phi, reco_etaAtVtx, reco_phiAtVtx, l1_pt, l1_eta, l1_phi, l1_etaAtVtx, l1_phiAtVtx, l1_ptCorr, l1_charge, l1_tfMuonIndex, l1_hwQual, dr_map, dphi
+		  outfile << std::setw(12) << reco_pt   << std::setw(12) << reco_eta  << std::setw(12)  << reco_phi       << std::setw(12) << reco_etaAtVtx <<  std::setw(15) << reco_phiAtVtx
+			  << std::setw(12) << l1_pt     << std::setw(12) << l1_eta    << std::setw(12)  << l1_phi         << std::setw(12) << l1_etaAtVtx   <<  std::setw(15) << l1_phiAtVtx
+			  << std::setw(12) << l1_ptCorr << std::setw(12) << l1_charge << std::setw(12)  << l1_tfMuonIndex << std::setw(12) << l1_hwQual 
+			  << std::setw(14) << dr_map    << std::setw(12) << dphi << std::endl;						
+		
+		  if (!onlytxt){
+		    //Store variables in vectors
+		    recomuon.Pt.push_back( reco_pt );
+		    recomuon.Eta.push_back( reco_eta );
+		    recomuon.Phi.push_back( reco_phi );
+		    recomuon.EtaAtVtx.push_back( reco_etaAtVtx );
+		    recomuon.PhiAtVtx.push_back( reco_phiAtVtx );
+		    recomuon.Charge.push_back( l1_charge );
+		    recomuon.HwQual.push_back( l1_hwQual );
+		    recomuon.Dr.push_back( dr_map );
+		    L1muon.Pt.push_back( l1_pt );
+		    L1muon.Eta.push_back( l1_eta );
+		    L1muon.PtCorr.push_back( l1_ptCorr );
+		    L1muon.Phi.push_back( l1_phi );
+		    L1muon.EtaAtVtx.push_back( l1_etaAtVtx );
+		    L1muon.PhiAtVtx.push_back( l1_phiAtVtx );
+		    L1muon.Charge.push_back( l1_charge );
+		    L1muon.TfMuonIndex.push_back( l1_tfMuonIndex );
+		    L1muon.HwQual.push_back( l1_hwQual );
+		  }
 		}
-		event_++;
-		recomuon_N = recomuon.Size(); L1muon_N = L1muon.Size(); 
-		if (!onlytxt) mytree->Fill();
+		
+		if (!onlytxt){
+		  event_i ++;
+		  event_.push_back(event_i);
+		  recomuon_N.push_back(dr_reco_L1.size()); 
+		  L1muon_N.push_back(dr_reco_L1.size());
+		}
 	}
 	outfile.close();
 	
-	//Write TTree in new Root file 
+	//Write TTree in root file 
 	if (!onlytxt){
-	  TString outRootName = out;
-	  outRootName.ReplaceAll(".txt",".root");
-	  TFile *outFileRoot = new TFile(outRootName,"RECREATE");
-	  std::cout<<"Created output file: "<<outRootName<<std::endl;
-	  outFileRoot->cd();
-	  mytree->Write();
-	  outFileRoot->Close();
+	  TTree * mytree =new TTree("mytree","mytree");	
+	  mytree->Branch("event",             &event_);
+	  mytree->Branch("recomuon_N",        &recomuon_N);
+	  mytree->Branch("recomuon_pt",       &recomuon.Pt);
+	  mytree->Branch("recomuon_eta",      &recomuon.Eta);
+	  mytree->Branch("recomuon_etaAtVtx", &recomuon.EtaAtVtx);
+	  mytree->Branch("recomuon_phi",      &recomuon.Phi);
+	  mytree->Branch("recomuon_phiAtVtx", &recomuon.PhiAtVtx);
+	  mytree->Branch("recomuon_dr",       &recomuon.Dr);
+	  mytree->Branch("L1muon_N",          &L1muon_N);
+	  mytree->Branch("L1muon_pt",         &L1muon.Pt);
+	  mytree->Branch("L1muon_ptCorr",     &L1muon.PtCorr);
+	  mytree->Branch("L1muon_eta",        &L1muon.Eta);
+	  mytree->Branch("L1muon_etaAtVtx",   &L1muon.EtaAtVtx);
+	  mytree->Branch("L1muon_phi",        &L1muon.Phi);
+	  mytree->Branch("L1muon_phiAtVtx",   &L1muon.PhiAtVtx);
+	  mytree->Branch("L1muon_charge",     &L1muon.Charge);
+	  mytree->Branch("L1muon_tfMuonIndex",&L1muon.TfMuonIndex);
+	  mytree->Branch("L1muon_hwQual",     &L1muon.HwQual);
+	  mytree->Fill();
+	  TString outName = out;
+	  outName.ReplaceAll(".txt",".root");
+	  TFile *outFile = new TFile(outName,"RECREATE");
+	  std::cout<<"Created output file: "<<outName<<std::endl;
+	  outFile -> cd();
+	  mytree  -> Write();
+	  outFile -> Close();
 	  std::cout<<"Output file written and closed!\n"<<std::endl;
 	}
 
 }
+
